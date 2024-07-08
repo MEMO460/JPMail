e912d9cdbf617a43329a1b9bde2acb2bc6d671b5
Mo'men Sobhy <67489376+MEMO460@users.noreply.github.com>
2024-07-08 06:11:14 +0300

    Add files via upload

---
+++ jpmail/plugins/gravatar/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+// Enable Gravatar by default
+$config['gravatar_enabled'] = true;
+
+// Set the size of the Gravatar image
+$config['gravatar_size'] = 128;
+
+// Set the rating of the Gravatar image (can be 'g', 'pg', 'r', 'x')
+$config['gravatar_rating'] = 'g';
+
+// Do not use a custom API
+$config['gravatar_custom'] = true;
+
+// Use HTTPS to fetch the Gravatar image
+$config['gravatar_https'] = true;
+
+// The Gravatar API URL
+$config['gravatar_photo_api'] = 'https://gravatar.com/mo2men5d90f9c419';
